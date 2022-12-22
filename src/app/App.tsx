634ba815098338d519d77dcc143f783dbfad3f33
pchmn <pchmn.dev@gmail.com>
2022-12-22 12:58:45 +0100

    feat: use unique sound

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,12 @@
 import JSConfetti from 'js-confetti';
-import { useState } from 'react';
 
 const jsConfetti = new JSConfetti();
 
-const joyeux = new Audio('sounds/joyeux.mp3');
-const noel = new Audio('sounds/noel.mp3');
-const grobin = new Audio('sounds/grobin.mp3');
-// const joyeuxNoel = new Audio('sounds/joyeux-noel-grobin.mp3');
+const joyeuxNoel = new Audio('sounds/joyeux-noel-grobin.mp3');
 
 function App() {
-  const [clickCount, setClickCount] = useState(0);
-
   const handleExplode = () => {
-    switch (clickCount) {
-      case 0:
-        joyeux.play();
-        break;
-      case 1:
-        noel.play();
-        break;
-      case 2:
-        grobin.play();
-        break;
-    }
-    // joyeuxNoel.play();
-
-    setClickCount((prev) => (prev === 2 ? 0 : prev + 1));
+    joyeuxNoel.play();
 
     jsConfetti.addConfetti({
       emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸']
