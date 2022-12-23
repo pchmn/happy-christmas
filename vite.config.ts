313dd50b6de1395f1d6ddfccd37cc8d2295cbbbe
pchmn <pchmn.dev@gmail.com>
2022-12-23 12:03:30 +0100

    build: init pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,42 @@
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/happy-christmas/',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'HappyChristmas',
+        short_name: 'HappyChristmas',
+        description: 'My Awesome App description',
+        background_color: '#201A1A',
+        theme_color: '#201A1A',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     sourcemap: true
-  },
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: './src/setupTests.ts',
-    coverage: {
-      reporter: ['html', 'lcov']
-    }
   }
 });
